c84979096910c291196df16684539fda88cf1325
marvin espira <espiramarvin@gmail.com>
2022-07-23 10:33:41 +0300

    rtk query performance optimization

---
+++ rtk-ex3-perf-optimization/src/App.js
@@ -0,0 +1,37 @@
+import AddPostForm from './features/posts/AddPostForm';
+import PostList from './features/posts/PostList';
+import SinglePostPage from './features/posts/SinglePostPage';
+import EditPostForm from './features/posts/EditPostForm';
+import UsersList from './features/users/UsersList';
+import UserPage from './features/users/UserPage';
+import Layout from './components/Layout';
+import { Routes, Route, Navigate} from 'react-router-dom'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+
+      <Route index element={<PostList />} />
+
+      <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
+      </Route>
+
+      <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+      </Route>
+
+      {/* catch all - replace with 404 component if yu want */}
+      <Route path="*" elememt={<Navigate to="/" replace />} />
+
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
